eec259ab4e8903ca2ef025c2fc307908231b1660
jusleen kaur rekhi <jusleenkaurrekhi@jusleens-MacBook-Pro.local>
2020-10-17 17:01:23 +0530

    Initial Commit

---
+++ src/test/java/TestSetup.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestSetup {
+
+    @Test
+    void testSetup() {
+
+        assertEquals(1,1);
+    }
+}
